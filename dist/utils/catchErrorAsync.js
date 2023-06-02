4a7b6dd5c27da64c2c73616d9ef3cf37f33f3a32
Nguyen Duong <muoi07052001@gmail.com>
2023-06-02 08:36:45 +0700

    Remove dist and yarn-error.log from git

--- dist/utils/catchErrorAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- *
- * @param {*} fn : asynchronous function
- * @return {function}: assigned to variable.
- */
-exports.default = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
